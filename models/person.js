20f1e0bcbb8ce2a1c0974c9636433e23587a21ad
Henrik Lyra <henrik.lyra@stat.fi>
2018-05-20 19:13:12 +0300

    3.21* eriytetty sovelluskehitys- ja tuotantotietokanta

--- models/person.js
+++ models/person.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose')
 
+if ( process.env.NODE_ENV !== 'production' ) {
+    console.log(process.env.NODE_ENV)
+    require('dotenv').config()
+}
+console.log(process.env.MONGODB_URI)
 const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
